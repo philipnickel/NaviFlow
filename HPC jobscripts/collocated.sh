d4b84a4ab6739d5a16873ab95c035b8b9f7b78d0
philipnickel <tipkik@hotmail.com>
2025-06-05 12:37:01 +0200

    results from hpc

--- HPC jobscripts/collocated.sh
+++ HPC jobscripts/collocated.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
-#BSUB -J cfd_array[1-100]               # 100 jobs in the array
-#BSUB -n 4                              # number of cores per task
-#BSUB -R "rusage[mem=4GB]"
-#BSUB -R "select[model==Intel_Platinum_8268]"  # Optional: request modern node
-#BSUB -W 02:00                          # 2 hours per job
-#BSUB -o logs/job_%J_%I.out
-#BSUB -e logs/job_%J_%I.err
+#BSUB -J collocated[1-55]              
+#BSUB -n 4                            
+#BSUB -R "rusage[mem=25GB]"
+#BSUB -W 03:00                        
+#BSUB -o logs/collocated_%J_%I.out
+#BSUB -e logs/collocated_%J_%I.err
+#BSUB -q hpc
+#BSUB -N
+#BSUB -u s214960@dtu.dk
+#BSUB -B
 
-# Activate environment
-source ~/.bashrc
+
+
+# Load modules or activate your environment
+module load latex/TexLive24
+
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate petsc_gpu_env
 
+# Change to project directory (assuming NaviFlow contains all scripts)
+cd ~/NaviFlow
+
+
 # Read config path for this job
-CONFIG=$(sed -n "${LSB_JOBINDEX}p" all_configs.txt)
+CONFIG=$(sed -n "${LSB_JOBINDEX}p" HPC\ jobscripts/collocated.txt)
 
 # Run simulation
 echo "Running config: $CONFIG"
--- HPC jobscripts/staggered.sh
+++ HPC jobscripts/staggered.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 #BSUB -J staggered[1-6]              # One task per script
-#BSUB -n 4                             # 1 core is likely enough
-#BSUB -R "rusage[mem=5GB]"
-#BSUB -W 02:00                         # Adjust as needed
+#BSUB -n 1                             # 1 core is likely enough
+#BSUB -R "rusage[mem=25GB]"
+#BSUB -W 05:00                         # Adjust as needed
 #BSUB -o logs/staggered_%J_%I.out
 #BSUB -e logs/staggered_%J_%I.err
+#BSUB -q hpc
+#BSUB -N
+#BSUB -u s214960@dtu.dk
+#BSUB -B
+
 
 # Load modules or activate your environment
-source ~/.bashrc
+module load latex/TexLive24
+
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate petsc_gpu_env
 
 # Change to project directory (assuming NaviFlow contains all scripts)
 cd ~/NaviFlow
 
 # Fetch the script path from the task file
-SCRIPT=$(sed -n "${LSB_JOBINDEX}p" staggered.txt)
+SCRIPT=$(sed -n "${LSB_JOBINDEX}p" HPC\ jobscripts/staggered.txt)
 
 echo "▶ Running: PYTHONPATH=$(pwd) python $SCRIPT"
 PYTHONPATH=$(pwd) python "$SCRIPT"
